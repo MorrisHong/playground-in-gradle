a0b5d0b493cc0ea7cce8bbaba316df6c04815dc4
Grace <govlmo91@gmail.com>
2020-09-20 05:38:56 +0900

    mastering-spring init

---
+++ mastering-spring/src/main/java/kr/gracelove/masteringspring/MasteringSpringApplication.java
@@ -0,0 +1,13 @@
+package kr.gracelove.masteringspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasteringSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasteringSpringApplication.class, args);
+	}
+
+}
---
+++ mastering-spring/src/test/java/kr/gracelove/masteringspring/MasteringSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.gracelove.masteringspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasteringSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
